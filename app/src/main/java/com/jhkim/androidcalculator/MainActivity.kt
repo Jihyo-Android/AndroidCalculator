47d64dc029f364decfb19a543a1e2c1681819c40
Jihyo Kim <wlgy33@naver.com>
2021-06-07 23:21:50 +0900

    Implement basic operation functions

--- app/src/main/java/com/jhkim/androidcalculator/MainActivity.kt
+++ app/src/main/java/com/jhkim/androidcalculator/MainActivity.kt
@@ -9,69 +9,87 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var result : Int = 0
-        var temp : String = ""
+        // new -> old (new + old)
+        var new = "0"
+        var old = "0"
 
-        if (result == 0) {
-            zero.setOnClickListener {
-                result += 0
-                equals.setText(result.toString())
-
-            }
-
-            one.setOnClickListener {
-                result += 1
-                equals.setText(result.toString())
-            }
-
-            two.setOnClickListener {
-                result += 2
-                equals.setText(result.toString())
-            }
-
-            three.setOnClickListener {
-                result += 3
-                equals.setText(result.toString())
-            }
-
-            four.setOnClickListener {
-                result += 4
-                equals.setText(result.toString())
-            }
-
-            five.setOnClickListener {
-                result += 5
-                equals.setText(result.toString())
-            }
-
-            six.setOnClickListener {
-                result += 6
-                equals.setText(result.toString())
-            }
-
-            seven.setOnClickListener {
-                result += 7
-                equals.setText(result.toString())
-            }
-
-            eight.setOnClickListener {
-                result += 8
-                equals.setText(result.toString())
-            }
-
-            nine.setOnClickListener {
-                result += 9
-                equals.setText(result.toString())
-            }
+        one.setOnClickListener {
+            new = new + "1"
+            result.setText(new)
+        }
+        two.setOnClickListener {
+            new = new + "2"
+            result.setText(new)
+        }
+        three.setOnClickListener {
+            new = new + "3"
+            result.setText(new)
+        }
+        four.setOnClickListener {
+            new = new + "4"
+            result.setText(new)
+        }
+        five.setOnClickListener {
+            new = new + "5"
+            result.setText(new)
+        }
+        six.setOnClickListener {
+            new = new + "6"
+            result.setText(new)
+        }
+        seven.setOnClickListener {
+            new = new + "7"
+            result.setText(new)
+        }
+        eight.setOnClickListener {
+            new = new + "8"
+            result.setText(new)
+        }
+        nine.setOnClickListener {
+            new = new + "9"
+            result.setText(new)
+        }
+        zero.setOnClickListener {
+            new = new + "0"
+            result.setText(new)
+        }
+        dot.setOnClickListener {
+            new = new + "."
+            result.setText(new)
         }
 
+        // Operations
         clear.setOnClickListener {
-            result = 0
-            equals.setText(result.toString())
+            new = "0"
+            old = "0"
+            result.setText("0")
         }
 
         plus.setOnClickListener {
-
+            old = (old.toInt() + new.toInt()).toString()
+            new = "0"
+            result.setText(old)
         }
+
+        minus.setOnClickListener {
+            old = (old.toInt() - new.toInt()).toString()
+            new = "0"
+            result.setText(old)
+        }
+
+        multiply.setOnClickListener {
+            old = (old.toInt() * new.toInt()).toString()
+            new = "0"
+            result.setText(old)
+        }
+
+        // improvement required
+        division.setOnClickListener {
+            old = (old.toInt() * new.toInt()).toString()
+            new = "0"
+            result.setText(old)
+        }
+
+
     }
 }
