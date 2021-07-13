2579bc25029e4d4f719b4cb4aa98ab62335fd8af
Jihyo Kim <wlgy33@naver.com>
2021-07-13 17:51:09 +0900

    Implement Stack
    Implement Stack for storing numbers and operators input

--- app/src/main/java/com/jhkim/androidcalculator/MainActivity.kt
+++ app/src/main/java/com/jhkim/androidcalculator/MainActivity.kt
@@ -9,6 +9,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var numStack: Stack<Int> =  Stack()
+        var operatorStack: Stack<String> = Stack()
+
+
+
         // new -> old (new + old)
         var new = ""
         var old = ""
@@ -18,45 +23,57 @@ class MainActivity : AppCompatActivity() {
             new = new + "1"
             result.setText(new)
         }
+
         two.setOnClickListener {
             new = new + "2"
             result.setText(new)
         }
+
         three.setOnClickListener {
             new = new + "3"
             result.setText(new)
         }
+
         four.setOnClickListener {
             new = new + "4"
             result.setText(new)
         }
+
         five.setOnClickListener {
             new = new + "5"
             result.setText(new)
         }
+
         six.setOnClickListener {
             new = new + "6"
             result.setText(new)
         }
+
         seven.setOnClickListener {
             new = new + "7"
             result.setText(new)
         }
+
         eight.setOnClickListener {
             new = new + "8"
             result.setText(new)
         }
+
         nine.setOnClickListener {
             new = new + "9"
             result.setText(new)
         }
+
         zero.setOnClickListener {
             new = new + "0"
             result.setText(new)
         }
+
         dot.setOnClickListener {
-            new = new + "."
-            result.setText(new)
+            if (!(old != "") && !("." in old)) {
+                new = new + "."
+                result.setText(new)
+            }
         }
 
         // Operations
@@ -64,6 +81,11 @@ class MainActivity : AppCompatActivity() {
             new = ""
             old = ""
             result.setText("")
+        }
+
+        backspace.setOnClickListener {
+            result.setText(old.substring(0, old.length - 1))
+            new = "0"
         }
 
         equals.setOnClickListener {
@@ -91,7 +113,44 @@ class MainActivity : AppCompatActivity() {
             new = "0"
             result.setText(old)
         }
-
-
     }
+}
+
+
+class Stack<T> {
+    var top: Int = -1
+    var capacity: Int = 0
+    var array = Array<Any>()
+
+    constructor(top: Int, capacity: Int, array: Array<Any>) {
+        this.top = top
+        this.capacity = capacity
+        this.array = array
+    }
+
+    fun isEmpty(): Boolean { return top == -1 }
+
+    fun isFull(): Boolean { return top == capacity - 1 }
+
+    fun push(data: Int) {
+        if (isFull()) println("스택 오버플로우 발생")
+        else array[++top] = data
+    }
+
+    fun pop(): Any {
+        if (isEmpty()) {
+            println("스택이 비어있습니다.")
+            return 0
+        }
+        else return array[top--]
+    }
+
+    fun init() { top = -1 }
+
+    fun display() {
+        for (item in array)
+            println(item)
+    }
+
+    fun deleteStack() { top = -1 }
 }
